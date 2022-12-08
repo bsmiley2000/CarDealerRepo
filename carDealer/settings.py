d2c0f0dccb6117385f692414a4f78c424ece6470
bsmiley2000 <bsmiley2000@gmail.com>
2022-12-08 14:16:12 -0700

    deployment

--- carDealer/settings.py
+++ carDealer/settings.py
@@ -82,16 +82,11 @@ WSGI_APPLICATION = 'carDealer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dealership',
-        'USER' : 'postgres',
-        'PASSWORD' : '',
-        'HOST' : 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
